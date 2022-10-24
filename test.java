d5ff72bae70a03d4fc5127293155ec81e1c32822
roushan <rkroushan9942@gmail.com>
2022-10-24 23:20:49 +0530

    first commit

---
+++ test.java
@@ -0,0 +1,12 @@
+package lockedme.com;
+
+public class test {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\hp\\Downloads\\roushan\\main"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+}
